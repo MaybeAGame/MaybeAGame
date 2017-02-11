64e0d4fdea5bf8eeaad14aa8f187f800722f9d44
grzegorz2047 <grzegorz2047@gmail.com>
2017-02-11 19:02:36 +0100

    first refactoring

--- core/src/pl/grzegorz2047/maybeagame/extension/ExtensionLoader.java
+++ core/src/pl/grzegorz2047/maybeagame/extension/ExtensionLoader.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,63 +23,85 @@ import java.util.zip.ZipInputStream;
 public class ExtensionLoader {
 
     public boolean loadExtensions() throws MalformedURLException {
-        File extensionsDirectory = new File("extensions");
-        if (!extensionsDirectory.isDirectory()) {
-            return extensionsDirectory.mkdir();
-        }
+        File extensionsDirectory = createExtensionsFileIfNotExists();
         File[] jars = extensionsDirectory.listFiles();
-        if (jars == null || jars.length == 0) return false;
+        if (isExtensionsDirectoryEmpty(jars)) return false;
         for (File jar : jars) {
-            if (!jar.getName().endsWith(".jar")) continue;
+            if (!hasJarExtension(jar)) continue;
             try {
                 ZipInputStream zip = new ZipInputStream(new FileInputStream(jar.getPath()));
                 JarFile jarFile = new JarFile(jar);
-                System.out.println("PATH: " + jar.getPath());
-                System.out.println("URI: " + jar.toURI());
-                System.out.println("URL: " + jar.toURI().toURL());
-                for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
-                    if (!entry.isDirectory() && entry.getName().equals("extension.info")) {
-                        Scanner scanner = new Scanner(jarFile.getInputStream(entry));
-                        if(!scanner.hasNextLine()){
-                            Logger.getLogger("MaybeAGame").log(Level.SEVERE, "Jar file named " + jar.getName() + " doesnt have extension.info");
-                            break;
-                        }
-                        String line = scanner.nextLine();
-                        String[] partsOfLine = line.split(":");
-                        String classPath = "";
-                        if(partsOfLine.length == 2){
-                            if(partsOfLine[0].equals("main")){
-                                classPath = partsOfLine[1];
-                            }
-                        }else {
-                            break;
-                        }
-                        System.out.println("Znalazlem plik!");
-                        URL url = jar.toURI().toURL();
-
-                        Class[] parameters = new Class[]{URL.class};
-
-                        URLClassLoader sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-                        Class<URLClassLoader> sysClass = URLClassLoader.class;
-                        try {
-                            Method method = sysClass.getDeclaredMethod("addURL", parameters);
-                            method.setAccessible(true);
-                            method.invoke(sysLoader, url);
-
-                            Constructor cs = ClassLoader.getSystemClassLoader().loadClass(classPath).getConstructor();
-                            Extension instance = (Extension) cs.newInstance();
-                            instance.onEnable();
-                        } catch (Exception ex) {
-                            System.err.println("Nie udalo sie wczytac " + jar.getName() + "! Sprawdz poprawnosc sciezki main! " + ex.getMessage());
-                            break;
-                        }
-                        break;
-                    }
-                }
+                getContentOfJarAndLoadIt(jar, zip, jarFile);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return true;
+    }
+
+    private void getContentOfJarAndLoadIt(File jar, ZipInputStream zip, JarFile jarFile) throws IOException {
+        for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
+            if (isItExtentionSettingFile(entry)) {
+                Scanner scanner = new Scanner(jarFile.getInputStream(entry));
+                if(!scanner.hasNextLine()){
+                    Logger.getLogger("MaybeAGame").log(Level.SEVERE, "Jar file named " + jar.getName() + " doesnt have extension.info");
+                    break;
+                }
+                String line = scanner.nextLine();
+                String[] partsOfLine = line.split(":");
+                String classPath = "";
+                if(partsOfLine.length == 2){
+                    if(partsOfLine[0].equals("main")){
+                        classPath = partsOfLine[1];
+                    }
+                }else {
+                    break;
+                }
+                System.out.println("Znalazlem plik!");
+                URL url = jar.toURI().toURL();
+
+                Class[] parameters = new Class[]{URL.class};
+
+                URLClassLoader sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+                Class<URLClassLoader> sysClass = URLClassLoader.class;
+                try {
+                    Extension instance = getInstanceOfExtension(classPath, url, parameters, sysLoader, sysClass);
+                    instance.onEnable();
+                } catch (Exception ex) {
+                    System.err.println("Nie udalo sie wczytac " + jar.getName() + "! Sprawdz poprawnosc sciezki main! " + ex.getMessage());
+                    break;
+                }
+                break;
+            }
+        }
+    }
+
+    private Extension getInstanceOfExtension(String classPath, URL url, Class[] parameters, URLClassLoader sysLoader, Class<URLClassLoader> sysClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException, InstantiationException {
+        Method method = sysClass.getDeclaredMethod("addURL", parameters);
+        method.setAccessible(true);
+        method.invoke(sysLoader, url);
+
+        Constructor cs = ClassLoader.getSystemClassLoader().loadClass(classPath).getConstructor();
+        return (Extension) cs.newInstance();
+    }
+
+    private boolean isItExtentionSettingFile(ZipEntry entry) {
+        return !entry.isDirectory() && entry.getName().equals("extension.info");
+    }
+
+    private boolean hasJarExtension(File jar) {
+        return jar.getName().endsWith(".jar");
+    }
+
+    private boolean isExtensionsDirectoryEmpty(File[] jars) {
+        return jars == null || jars.length == 0;
+    }
+
+    private File createExtensionsFileIfNotExists() {
+        File extensionsDirectory = new File("extensions");
+        if (!extensionsDirectory.isDirectory()) {
+            extensionsDirectory.mkdir();
+        }
+        return extensionsDirectory;
     }
 }
