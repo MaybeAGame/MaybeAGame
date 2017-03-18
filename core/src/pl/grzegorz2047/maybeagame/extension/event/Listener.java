e0c22d76a33a7771d17997a49441a489919fddb4
grzegorz2047 <grzegorz2047@gmail.com>
2017-03-18 22:00:56 +0100

    Dodanie generyka.

--- core/src/pl/grzegorz2047/maybeagame/extension/event/Listener.java
+++ core/src/pl/grzegorz2047/maybeagame/extension/event/Listener.java
@@ -3,8 +3,8 @@ package pl.grzegorz2047.maybeagame.extension.event;
 /**
  * Plik stworzony przez grzegorz2047 11.02.2017.
  */
-public interface Listener {
+public interface Listener<T extends Event> {
 
-    void notify(Event e);
+    void notify(T event);
 
 }
